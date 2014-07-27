8253a557ebfa5ed379f9996c13209f39192f39f3
Pujun Bhatnagar <pbhatnagar3@gatech.edu>
2014-07-26 21:02:41 -0700

    TrailingZerosProblem

---
+++ src/ComputingTrailingZeros.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * Computing the number of trailing zeros of a factorial
+ * @author pbhatnagar
+ *
+ */
+public class ComputingTrailingZeros {
+
+	public int numberOfTrailingZeros(int num){
+		int count = 0;
+		for(int i = 0; i < num; i++){
+			count += noOfFives(i);
+		}
+		
+		return count;
+	}
+	
+	public int noOfFives(int num){
+		int factor = 5;
+		int count = 0;
+		while(true){
+			count += num/factor;
+			factor *= 5;
+			if(factor > num)
+				break;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number for which you want to find the number of trailing zeros");
+		int num = scan.nextInt();
+		ComputingTrailingZeros compute = new ComputingTrailingZeros();
+		System.out.println("The number of trailing zeros in the factorial is " + compute.noOfFives(num));
+	}
+}
