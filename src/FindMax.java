445e81997ea3e9342bde3c5e94fd61f962995b54
Pujun Bhatnagar <pbhatnagar3@gatech.edu>
2014-07-26 21:30:38 -0700

    awesome comparison

---
+++ src/FindMax.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Trying to find the maximum of two numbers without using any comparison. Now that is interesting :D
+ * @author pbhatnagar
+ * NOTE: this does not take care of the overflow error
+ *
+ */
+public class FindMax {
+	final static int MACHINE_BIT = 31;
+	//The following would return 1 if the number is positive and 0 when the number is negative
+	public int findDSign(int num){
+//		System.out.println(Integer.toBinaryString(num) +  Integer.toBinaryString(num).length());
+//		System.out.println((num >> MACHINE_BIT) & 1);
+		return ((num >> MACHINE_BIT) & 1) ^ 1; 
+	}
+	
+	public int findMax(int a, int b){
+		
+		int k = findDSign(a-b);
+		int q = k^1;
+		return k * a + q * b;
+	}
+	
+	public static void main(String[] args) {
+		//this variable is cool because, without using any comparison, it finds the max
+		//which is awesome. PS: I am getting bored on a weekend and this is what I will name it. :P :D
+		FindMax cool = new FindMax();
+		System.out.println("Enter two numbers");
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.println("The bigger number is " + cool.findMax(a, b));
+	}
+}
